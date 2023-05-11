6c630a75bef6503ec1172569a86db15b3a1cd7c4
adriansanchezz <114017148+adriansanchezz@users.noreply.github.com>
2023-05-11 13:53:24 +0200

    Añadido carrito a la empresa.

--- pag/empresa.php
+++ pag/empresa.php
@@ -27,10 +27,17 @@ require_once '../lib/modulos.php';
                 </li>
                 <li>
                     <form action="empresa.php" method="post">
+                        <button type="submit" name="empresaCarrito"
+                            class="btn btn-link nav-link text-white">Carrito</button>
+                    </form>
+                </li>
+                <li>
+                    <form action="empresa.php" method="post">
                         <button type="submit" name="empresaVigia"
                             class="btn btn-link nav-link text-white">Informacion</button>
                     </form>
                 </li>
+
             </ul>
         </div>
 
@@ -61,22 +68,22 @@ require_once '../lib/modulos.php';
 
             // Verificar si el carrito de compras ya está almacenado en la sesión
             if (isset($_SESSION['empresaCart'])) {
-                $cart = $_SESSION['empresaCart'];
+                $empresaCart = $_SESSION['empresaCart'];
             } else {
-                $cart = array(); // Crear un nuevo carrito si no existe
+                $empresaCart = array(); // Crear un nuevo carrito si no existe
             }
 
             // Verificar si el producto ya existe en el carrito
-            if (isset($cart[$product_id])) {
+            if (isset($empresaCart[$product_id])) {
                 // Incrementar la cantidad del producto en 1
-                $cart[$product_id]++;
+                $empresaCart[$product_id]++;
             } else {
                 // Agregar el producto al carrito con una cantidad de 1
-                $cart[$product_id] = 1;
+                $empresaCart[$product_id] = 1;
             }
 
             // Guardar el carrito actualizado en la sesión
-            $_SESSION['empresaCart'] = $cart;
+            $_SESSION['empresaCart'] = $empresaCart;
 
             header("Location: empresa.php?empresaMapa=1");
             exit();
@@ -92,51 +99,45 @@ require_once '../lib/modulos.php';
                 <?php
                 // Verificar si el carrito de compras está almacenado en la sesión
                 if (isset($_SESSION['empresaCart'])) {
-                    $cart = $_SESSION['empresaCart'];
+                    $empresaCart = $_SESSION['empresaCart'];
 
-                    // Consultar las propiedades desde la base de datos utilizando los IDs en el carrito
-                    $productIds = array_keys($cart);
+                    // Consultar los productos desde la base de datos utilizando los IDs en el carrito
+                    // Consultar los productos desde la base de datos utilizando los IDs en el carrito
+                    $productIds = array_keys($empresaCart);
+
                     if (!empty($productIds)) {
                         $productIdsString = implode(",", $productIds);
-                        if (!empty($productIdsString)) {
-                            echo "IDs de propiedades: $productIdsString";
-                            // ... Resto de tu código ...
-                        } else {
-                            echo "La lista de IDs de propiedades está vacía";
-                        }
-                        $sql = "SELECT * FROM `propiedades` WHERE id_propiedad IN ($productIdsString)";
+                        $productIdsString = trim($productIdsString, ", ");
+
+                        $sql = "SELECT * FROM propiedades WHERE id_propiedad IN ($productIdsString)";
                         $conn = conectar();
-                        if (!$conn) {
-                            die("Error de conexión a la base de datos: " . mysqli_connect_error());
-                        }
                         $result = $conn->query($sql);
 
+
+
                         if ($result->num_rows > 0) {
-                            // Iterar sobre las propiedades en el carrito
+                            // Iterar sobre los productos en el carrito
                             while ($row = $result->fetch_assoc()) {
                                 $product_id = $row['id_propiedad'];
-                                $product_name = $row['provincia'];
-                                $product_description = $row['ubicacion'];
-                                $product_price = $row['codigo_postal'];
-                                $product_quantity = $cart[$product_id];
+                                $product_name = $row['ubicacion'];
+                                $product_description = $row['provincia'];
+                                $product_price = $row['precio'];
 
-                                // Calcular el subtotal por propiedad
+                                // Calcular el subtotal por producto
                                 $subtotal = $product_price * $product_quantity;
 
                                 // Agregar el subtotal al total de dinero
                                 $totalMoney += $subtotal;
 
-                                // Mostrar los detalles de la propiedad en el carrito
+                                // Mostrar los detalles del producto en el carrito
                                 echo "<div class='product card border-primary mb-3' style='max-width: 18rem;'>";
                                 echo "<div class='card-header bg-primary text-white'>$product_name</div>";
                                 echo "<div class='card-body text-primary'>";
                                 echo "<h5 class='card-title'>$product_description</h5>";
                                 echo "<p class='card-text'>Precio: $product_price</p>";
-                                echo "<p class='card-text'>Cantidad: $product_quantity</p>";
-                                echo "<p class='card-text'>Subtotal: $subtotal</p>";
-                                echo "<form action='empresa.php' method='post'>";
+                                echo "<form action='usuario.php' method='post'>";
                                 echo "<input type='hidden' name='product_id' value='$product_id'>";
-                                echo "<button class='btn btn-danger' name='remove_from_cart' value='$product_id' type='submit'>Eliminar</button>";
+                                echo "<button class='btn btn-danger' name='remove_from_empresaCart' type='submit'>Eliminar</button>";
                                 echo "</form>";
                                 echo "</div>";
                                 echo "</div>";
@@ -144,7 +145,9 @@ require_once '../lib/modulos.php';
                         }
                         mysqli_close($conn);
                     }
+
                 }
+
 
                 // Mostrar el total de dinero en el carrito
                 echo "<p>Total a pagar: $totalMoney €</p>";
