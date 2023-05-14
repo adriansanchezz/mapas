a243d9b8e14f6fecb1939af37d313fd9b839503c
adriansanchezz <114017148+adriansanchezz@users.noreply.github.com>
2023-05-14 10:58:19 +0200

    Organizado mapa vigilante.

--- lib/modulos.php
+++ lib/modulos.php
@@ -498,7 +498,7 @@ function mapa($valor)
 
                             // Consultar los marcadores dentro del límite
                             // Reemplaza esta sección con el código PHP correspondiente
-                            fetch('get_markers.php', {
+                            fetch('../lib/modulos.php', {
                                 method: 'POST',
                                 headers: {
                                     'Content-Type': 'application/json'
@@ -514,7 +514,6 @@ function mapa($valor)
                                     return response.json();
                                 })
                                 .then(function (markers) {
-
                                     markers.forEach(function (marker) {
                                         var lat = marker.latitud;
                                         var lng = marker.longitud;
@@ -527,7 +526,6 @@ function mapa($valor)
                                         // Crear un marcador para cada registro de la base de datos dentro del límite
                                         var marker = L.marker([lat, lng]).addTo(map);
                                     })
-
                                 })
                                 .catch(function (error) {
                                     console.log("Error al obtener los marcadores: " + error.message);
@@ -542,6 +540,7 @@ function mapa($valor)
                     }
                 </script>
 
+
             </div>
         </div>
         <?php
@@ -549,4 +548,52 @@ function mapa($valor)
     }
 
 }
+?>
+
+
+<?php
+function mapaVigilante()
+{
+    require_once '../lib/functiones.php';
+    require_once '../lib/modulos.php';
+    // Obtener los valores enviados desde JavaScript
+    $data = json_decode(file_get_contents('php://input'), true);
+    $latMin = $data['latMin'];
+    $lngMin = $data['lngMin'];
+    $latMax = $data['latMax'];
+    $lngMax = $data['lngMax'];
+
+    // Establecer la conexión con la base de datos
+    $conn = conectar();
+
+    // Verificar la conexión
+    if ($conn->connect_error) {
+        die("Error de conexión a la base de datos: " . $conn->connect_error);
+    }
+
+    // Consultar los marcadores existentes dentro del límite
+    $sql = "SELECT * FROM propiedades WHERE latitud BETWEEN ? AND ? AND longitud BETWEEN ? AND ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("dddd", $latMin, $latMax, $lngMin, $lngMax);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    $markers = array();
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $markers[] = $row;
+        }
+    }
+
+    // Cerrar la conexión a la base de datos
+    $stmt->close();
+    $conn->close();
+
+    // Devolver los marcadores como respuesta en formato JSON
+    header('Content-Type: application/json');
+    echo json_encode($markers);
+}
+
+// Llamar a la función mapaVigilante para que se ejecute al acceder a este archivo
+mapaVigilante();
 ?>
