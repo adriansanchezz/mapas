43e3f5207db9d5998284422a0af5b6048d909ba6
KanBinL <klou01@educantabria.es>
2023-06-08 16:18:24 +0200

    Mejorar diseño menu

--- pag/principal.php
+++ pag/principal.php
@@ -13,17 +13,7 @@ require_once '../lib/modulos.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-    <style>
-        /* Mantener la relación de aspecto de las imágenes */
-        .carousel-inner img {
-            object-fit: contain;
-            height: 250px;
-        }
 
-        .carousel {
-            width: 40vh;
-        }
-    </style>
 </head>
 
 <body>
